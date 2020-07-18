ee70866d7a5d71a9927abc050529e621504b7620
Dima Antonenko <daemondw@gmail.com>
2020-07-18 21:20:39 +0200

    oxygen

--- Kapadokiya/js/main.js
+++ Kapadokiya/js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true
+    });
 });
