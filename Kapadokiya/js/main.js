6071edb961282d2b74367bc7212ba12c573f652c
Dima Antonenko <daemondw@gmail.com>
2020-07-20 17:17:09 +0200

    finish Kapp

--- Kapadokiya/js/main.js
+++ Kapadokiya/js/main.js
@@ -4,6 +4,27 @@ $(function () {
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
         autoplay: true,
         autoplaySpeed: 2000,
-        fade: true
+        fade: true,
+        responsive: [{
+            breakpoint: 601,
+            settings: {
+                arrows: false
+            }
+        }]
+    });
+    $(".menu, .our-trip__body").on("click", "a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
     });
 });
